3dcbf3c0f522428be7b8f80c5998885d2bdf6108
rishisun <rishisun99@gmail.com>
2017-03-07 14:55:53 -0600

    added 6march

--- src/Feb_22/WordLength.java
+++ src/Feb_22/WordLength.java
@@ -22,10 +22,10 @@ public class WordLength {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String words[] = { "zero ", "one ", "two ", "there ", "four ", "five ", "six ", "seven ", "eigth ", "nine ",
-				"ten ", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixtenn", "seventeen", "eigteen",
+				"ten ", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixtenn", "seventeen", "eighteen",
 				"nineteen", "twenty ", "thirty ", "fourty ", "fifty ", "sixty ", "seventy ", "eighty ", "ninty ",
 				"hundred ", "thousand ", "and " };
-		int count[] = { 4, 3, 3, 5, 4, 4, 3, 5, 5, 4, 3, 6, 6, 8, 8, 7, 7, 9, 8, 8, 6, 6, 6, 5, 5, 7, 6, 6, 7, 8, 3 };
+		int count[] = { 4, 3, 3, 5, 4, 4, 3, 5, 5, 4, 3, 6, 6, 8, 8, 7, 7, 9, 9, 8, 6, 6, 6, 5, 5, 7, 6, 6, 7, 8, 3 };
 		int n = 0, temp = 0, tens, hundreds, thousand, ones;
 		System.out.println("enter the positive number till thousand");
 		Scanner x = new Scanner(System.in);
@@ -40,8 +40,14 @@ public class WordLength {
 			tens = (tens - 2) + 20;
 			ones = temp % 10;
 			System.out.print(n + " : ");
-			System.out.println(words[tens] + words[ones]);
-			System.out.println(count[tens] + count[ones]);
+			if (ones != 0) {
+				System.out.println(words[tens] + words[ones]);
+				System.out.println(count[tens] + count[ones]);
+			} else {
+				System.out.println(words[tens]);
+				System.out.println(count[tens]);
+			}
+
 		} else if (n < 1000) {
 			hundreds = temp / 100;
 			temp = temp - hundreds * 100;
@@ -57,28 +63,28 @@ public class WordLength {
 				System.out.println(count[hundreds] + count[28] + count[30] + count[tens]);
 
 			}
-			}
-			else{
-				thousand = temp/1000;
-				temp =temp-thousand*1000;
-				hundreds = temp / 100;
-				temp = temp - hundreds * 100;
-				tens = (temp / 10);
-				tens = (tens - 2) + 20;
-				ones = temp % 10;
-				System.out.print(n + " : ");
-				if (ones != 0) {
-					System.out.println(words[thousand]+words[30]+words[hundreds] + words[28] + words[30] + words[tens] + words[ones]);
-					System.out.println(count[thousand]+count[30]+count[hundreds] + count[28] + count[30] + count[tens] + count[ones]);
-				} else {
-					System.out.println(words[thousand]+words[30]+words[hundreds] + words[28] + words[30] + words[tens]);
-					System.out.println(count[thousand]+count[30]+count[hundreds] + count[28] + count[30] + count[tens]);
+		} else {
+			thousand = temp / 1000;
+			temp = temp - thousand * 1000;
+			hundreds = temp / 100;
+			temp = temp - hundreds * 100;
+			tens = (temp / 10);
+			tens = (tens - 2) + 20;
+			ones = temp % 10;
+			System.out.print(n + " : ");
+			if (ones != 0) {
+				System.out.println(words[thousand] + words[30] + words[hundreds] + words[28] + words[30] + words[tens]
+						+ words[ones]);
+				System.out.println(count[thousand] + count[30] + count[hundreds] + count[28] + count[30] + count[tens]
+						+ count[ones]);
+			} else {
+				System.out.println(words[thousand] + words[30] + words[hundreds] + words[28] + words[30] + words[tens]);
+				System.out.println(count[thousand] + count[30] + count[hundreds] + count[28] + count[30] + count[tens]);
 
-				}
-
 			}
-		
+
+		}
 
 		x.close();
-	}}
-
+	}
+}
